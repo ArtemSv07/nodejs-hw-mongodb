cd147a06c115d2e599fcdcbeb13f77317b64ff91
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-06-20 23:25:47 +0300

    hw-4

---
+++ src/validation/students.js
@@ -0,0 +1,39 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(30).required().messages({
+    'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+    'any.required': 'phoneNumber is required',
+  }),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean().messages({
+    'string.base': 'isFavourite should be a boolean',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.min': 'phoneNumber should have at least {#limit} characters',
+    'string.max': 'phoneNumber should have at most {#limit} characters',
+    'any.required': 'phoneNumber is required',
+  }),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean().messages({
+    'string.base': 'isFavourite should be a boolean',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
